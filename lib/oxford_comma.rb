6dabbabd417d59951bd79907e0c31734407a0530
Bobbi York <york1670@gmail.com>
2018-09-29 04:36:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,10 @@
 def oxford_comma(array)
-  return nil if array.nil?
-  return array[0] if array.count == 1
-  return array.join(" and ") if array.count == 2
-  return array[0..-2].join(', ') + ", and " + array[-1] if array.count > 1
+  if array.nil?
+    return nil 
+  elsif array.count == 1
+    return array[0]   
+  elsif array.count == 2
+    return array.join(" and ") 
+  if array.count >= 3
+    return array[0..-1].join(', ') + " and " + array[-1] 
 end
