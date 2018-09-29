ccee022e5e5de1c4a106d2caccf842eee5b8fcf9
Bobbi York <york1670@gmail.com>
2018-09-29 04:36:43 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
   if array.nil?
-    return nil 
+    return nil
   elsif array.count == 1
-    return array[0]   
+    return array[0]
   elsif array.count == 2
-    return array.join(" and ") 
+    return array.join(" and ")
   if array.count >= 3
-    return array[0..-1].join(', ') + " and " + array[-1] 
+    return array[0..-1].join(', ') + " and " + array[-1]
 end
